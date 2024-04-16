df5ea5db34c5368897712f8e682d395b85707ebb
Fabian <fabianpon@gmail.com>
2024-04-15 19:02:25 -0700

    Ruteo estatico HomePage

--- fronted/src/app/page.js
+++ fronted/src/app/page.js
@@ -1,10 +1,10 @@
 import { Phone, Mail, MapPin } from 'react-feather';
-import Servicios from "./servicios";
-import Contactanos from './contactanos';
-import NuestroEquipo from './nuestro-equipo';
-import SobreNosotros from './sobre-nosotros';
 import Header from '@/components/Header';
 import Banner from '@/components/Banner';
+import Contactanos from './pages/contactanos';
+import NuestroEquipo from './pages/nuestro-equipo';
+import SobreNosotros from './pages/sobre-nosotros';
+import Servicios from './pages/servicios';
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen min-w-full md:min-w-0 bg-primary-main mx-24">
@@ -22,10 +22,19 @@ export default function Home() {
           <Banner />
         </div>
       </section>
-      <Servicios />
-      <SobreNosotros />
-      <NuestroEquipo />
-      <Contactanos />
+      <section id='servicios' className="flex flex-col items-center min-h-screen min-w-full justify-between bg-primary-seccion">
+        <Servicios />
+      </section>
+      <section id='sobre-nosotros' className="flex flex-col items-center min-h-screen min-w-full justify-between bg-primary-seccion">
+        <SobreNosotros />
+      </section>
+      <section id='nuestro-equipo' className="flex flex-col items-center min-h-screen min-w-full justify-between bg-primary-seccion">
+        <NuestroEquipo />
+      </section>
+      <section id='contactanos' className="flex flex-col items-center min-h-screen min-w-full justify-between bg-primary-seccion">
+        <Contactanos />
+      </section>
+
     </main>
   );
 }
