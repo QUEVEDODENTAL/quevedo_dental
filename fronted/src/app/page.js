46b5a8dd123ef6da85fa801df8a456f4d4a233eb
Fabian <fabianpon@gmail.com>
2024-04-15 18:23:18 -0700

    Correccion de errores

--- fronted/src/app/page.js
+++ fronted/src/app/page.js
@@ -1,25 +1,31 @@
 import { Phone, Mail, MapPin } from 'react-feather';
-import HeroBanner from "./pages";
 import Servicios from "./servicios";
 import Contactanos from './contactanos';
 import NuestroEquipo from './nuestro-equipo';
 import SobreNosotros from './sobre-nosotros';
+import Header from '@/components/Header';
+import Banner from '@/components/Banner';
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen min-w-full md:min-w-0 bg-primary-main mx-24">
       <div className="flex items-center min-w-full justify-between p-2 bg-primary-info">
-        <div className="p-3"><p className="text-sm flex text-text-blackText"><MapPin size={16} className='mx-2'/> Valle dorado, valle de los sirios, Ensenada, Baja California</p></div>
+        <div className="p-3"><p className="text-sm flex text-text-blackText"><MapPin size={16} className='mx-2' /> Valle dorado, valle de los sirios, Ensenada, Baja California</p></div>
         <div className="flex">
-          <div className="p-3"><p className="text-sm flex"><Phone size={16} className='mx-2'/> +52 646 256 3625</p></div>
+          <div className="p-3"><p className="text-sm flex"><Phone size={16} className='mx-2' /> +52 646 256 3625</p></div>
           <div className="p-3">|</div>
           <div className="p-3"><p className="text-sm flex"><Mail size={16} className='mx-2' /> doctor@consultoriomx.com</p></div>
         </div>
       </div>
-      <HeroBanner></HeroBanner>
-      <Servicios/>
-      <SobreNosotros/>
-      <NuestroEquipo/>
-      <Contactanos/>
+      <section className="flex flex-col items-center min-h-screen min-w-full justify-between bg-primary-seccion">
+        <Header />
+        <div className="flex flex-row justify-center">
+          <Banner />
+        </div>
+      </section>
+      <Servicios />
+      <SobreNosotros />
+      <NuestroEquipo />
+      <Contactanos />
     </main>
   );
 }
