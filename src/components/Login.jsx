85e0a15a7424cfed80f3f4bd4abb32831eb0b239
Fabian <fabianpon@gmail.com>
2024-04-16 17:22:07 -0700

    Actualizcion de RegistroPage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,28 @@
-import Link from "next/link"
+'use client'
+
+import Link from "next/link";
 
 const Login = () => {
 
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const correo = event.target.correo.value;
+        const password = event.target.password.value;
+        console.log("Correo:", correo);
+        console.log("Contraseña:", password);
+    };
+
     return (
         <div className="mx-1 my-1 p-4 border-2 rounded-[15px] drop-shadow-2xl ">
-            <h1 className='text-5xl font-semibold flex justify-center mb-[30px]'>Iniciar Sesion</h1>
+            <h1 className='text-5xl font-semibold flex justify-center mb-[30px]'>Iniciar Sesión</h1>
 
-            <form>
+            <form onSubmit={handleLogin}>
                 <label className="text-lg font-medium px-[12px]">Correo</label>
                 <input
                     type="email"
                     name='correo'
                     className="w-full outline-none rounded-[25px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
-                    placeholder="Ingresa tu correo electronico" />
+                    placeholder="Ingresa tu correo electrónico" />
                 <label className="text-lg font-medium px-[12px]">Contraseña</label>
                 <input
                     type="password"
@@ -20,20 +30,21 @@ const Login = () => {
                     className="w-full outline-none rounded-[25px]  bg-[#e4e4e4] px-[12px] py-[15px]"
                     placeholder="Ingresa tu contraseña" />
 
+                <div className="mt-8 flex flex-col">
+                    <button
+                        type='submit'
+                        name='registrar'
+                        className="text-[1rem] p-[10px] rounded-[25px] bg-[#1f5792] cursor-pointer text-white" >
+                        Iniciar sesión
+                    </button>
+                </div>
             </form>
-            <div className="mt-8 flex flex-col">
-                <button
-                    type='submit'
-                    name='registrar'
-                    className="text-[1rem] p-[10px] rounded-[25px] bg-[#1f5792] cursor-pointer text-white" >
-                    Iniciar secion
-                </button>
-            </div>
             <div className="flex justify-center mt-[15px]">
-                <p className="mr-2">No estas registrado?</p>
-                <Link href={'/admin/registrar'} className="text-blue-500">Registrate Ahora</Link>
+                <p className="mr-2">¿No estás registrado?</p>
+                <Link href={'/admin/registrar'} className="text-blue-500">Regístrate Ahora</Link>
             </div>
         </div>
     )
-}
-export default Login
+};
+
+export default Login;
--- src/components/Registrar.jsx
+++ src/components/Registrar.jsx
@@ -1,12 +1,16 @@
 const RegistrarPage = () => {
 
     return (
-        <div className="bg-white mx-1 my-1 p-4  border-2 rounded-[15px] drop-shadow-2xl lg:w-1/4 md:w-1/2">
-            <h1 className='text-5xl font-semibold flex justify-center mb-[30px]'>Registrar</h1>
+        <div className=" mx-1 my-1 p-4 border-10 custom-shadowa rounded-[15px] lg:w-4/5 md:w-1/2">
+            <h1 className='text-5xl font-bold flex justify-center mb-[30px]'>Registro</h1>
 
             <div className="flex justify-around">
-                <button>Doctor </button>
-                <button>Empleado</button>
+                <div>
+                    <button className="font-semibold text-secondary-font hover:bg-cyan-600" >Doctor </button>
+                </div>
+                <div>
+                    <button className=" font-semibold text-secondary-font">Empleado</button>
+                </div>
             </div>
 
             <form>
@@ -14,41 +18,32 @@ const RegistrarPage = () => {
                 <input
                     type="text"
                     name='nombre'
-                    className="w-full outline-none rounded-[25px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
-                    placeholder="Ingresa tu nombre" />
-                <label className="text-lg font-medium px-[12px]">Apellido paterno</label>
+                    className="w-full outline-secondary-font rounded-[5px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
+                    placeholder="Nombre" />
+                <label className="text-lg font-medium px-[12px]">Apellidos</label>
                 <input
                     type="text"
-                    name='apellidop'
-                    className="w-full outline-none rounded-[25px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
-                    placeholder="Ingresa tu apellido paterno" />
-                <label className="text-lg font-medium px-[12px]">Apellido materno</label>
-                <input
-
-                    type="text"
-                    name='apellidom'
-                    className="w-full outline-none rounded-[25px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
-                    placeholder="Ingresa tu apellido materno" />
+                    name='Apellidos'
+                    className="w-full outline-none rounded-[5px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
+                    placeholder="Apellidos" />
                 <label className="text-lg font-medium px-[12px]">Correo</label>
                 <input
 
                     type="email"
                     name='correo'
-                    className="w-full outline-none rounded-[25px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
+                    className="w-full outline-none rounded-[5px] mb-[10px] bg-[#e4e4e4] px-[12px] py-[15px]"
                     placeholder="Ingresa tu correo electronico" />
                 <label className="text-lg font-medium px-[12px]">Contraseña</label>
                 <input
                     type="password"
                     name='password'
-                    className="w-full outline-none rounded-[25px] bg-[#e4e4e4] px-[12px] py-[15px]"
+                    className="w-full outline-none rounded-[5px] bg-[#e4e4e4] px-[12px] py-[15px]"
                     placeholder="Ingresa tu contraseña" />
-
             </form>
-            <div className="mt-8 flex flex-col">
-                <button
-                    type='submit'
-                    name='registrar'
-                    className="text-[1rem] p-[10px] rounded-[25px] bg-[#212529] cursor-pointer text-white" >Registrar</button>
+            <div className="mt-8 flex flex-col ">
+                <button type="submit" name="registrar" className="text-[1rem] p-[10px] rounded-[10px] bg-primary-info text-primary-seccion cursor-pointer text-white hover:bg-primary-info hover:text-primary-seccion hover:scale-95 transition-transform duration-300">
+                    Registrar
+                </button>
             </div>
         </div>
     )
