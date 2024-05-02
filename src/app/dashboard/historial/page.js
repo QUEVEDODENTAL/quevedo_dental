2f13b207455de15340f5e2be2ed7fb47c0cb1944
LuisMurSal <al19760547@ite.edu.mx>
2024-05-02 09:26:23 -0700

    Arreglando informacion de las rutas

--- src/app/dashboard/historial/page.js
+++ src/app/dashboard/historial/page.js
@@ -1,25 +1,58 @@
-// pages/dashboard/pages/historial.js
 import React from 'react';
+import { FaUserAlt, FaBookMedical, FaLock, FaBars, FaIdCardAlt, FaCalendarAlt } from "react-icons/fa";
+import { FaHouse, FaAddressBook, FaAddressCard, FaHospitalUser } from "react-icons/fa6";
 
-const HistorialPage = () => {
-  const historialData = [
-    { id: 1, date: '2024-04-01', description: 'Consulta médica' },
-    { id: 2, date: '2024-04-15', description: 'Análisis de sangre' },
-    { id: 3, date: '2024-04-30', description: 'Seguimiento del tratamiento' },
-  ];
-
+const Dashboard = () => {
   return (
-    <div>
-      <h1>Historial</h1>
-      <ul>
-        {historialData.map(item => (
-          <li key={item.id}>
-            <strong>{item.date}</strong>: {item.description}
-          </li>
-        ))}
-      </ul>
+    <div className="h-screen flex flex-col justify-start items-center md:pl-8">
+      <h1 className="text-3xl font-bold mb-4 mt-4">¡Historial!</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="relative bg-primary-info p-4 text-center rounded-lg">
+        <FaAddressCard className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold mb-2">Pacientes</h2>
+          <p>Ver datos de los</p>
+          <p>pacientes</p>
+        </div>
+        <div className="relative bg-secondary-font p-4 text-center rounded-lg">
+        <FaAddressBook className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold mb-2">Historial</h2>
+          <p>Ver historial de las citas</p>
+          <p>realizadas</p>
+        </div>
+        <div className="relative bg-primary-red p-4 text-center rounded-lg">
+        <FaCalendarAlt className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold mb-2">Agenda</h2>
+          <p>Presiona para agendar</p>
+          <p>o modificar alguna cita</p>
+        </div>
+        <div className="relative bg-secondary-icon p-4 text-center rounded-lg hidden md:block">
+        <FaAddressCard className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold">Ultimo</h2>
+          <h2 className="text-xl font-bold mb-2">paciente</h2>
+          <p>Ver datos del</p>
+          <p>ultimo paciente</p>
+        </div>
+        <div className="relative bg-secondary-button p-4 text-center rounded-lg hidden md:block">
+        <FaBookMedical className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold">Consulta</h2>
+          <h2 className="text-xl font-bold mb-2">siguiente</h2>
+          <p>Contenido de la siguiente</p>
+          <p>consulta</p>
+        </div>
+        <div className="relative bg-secondary-icon p-4 text-center rounded-lg hidden md:block">
+        <FaIdCardAlt className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold mb-2">Cuestionario</h2>
+          <p>Realizar cuestionario</p>
+          <p>Para el paciente</p>
+        </div>
+        <div className="relative col-span-3 bg-primary-info p-4 text-center rounded-lg hidden md:block">
+        <FaHospitalUser className="absolute top-0 left-0 mt-2 ml-2" size={30} />
+          <h2 className="text-xl font-bold mb-2">Registrar a nuevo doctor o empleado</h2>
+          <p>Presiona para registrar a nuevo doctor o empleado</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default HistorialPage;
+export default Dashboard;
--- src/components/Sliderbar.jsx
+++ src/components/Sliderbar.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { FaUserAlt, FaBookMedical, FaLock, FaBars } from "react-icons/fa";
+import { FaUserAlt, FaBookMedical, FaLock, FaBars, FaIdCardAlt } from "react-icons/fa";
 import { FaHouse, FaAddressBook, FaAddressCard, FaHospitalUser } from "react-icons/fa6";
 
 const SliderBar = () => {
@@ -13,11 +13,9 @@ const SliderBar = () => {
 
   return (
     <div>
-      {!isSliderVisible && (
-        <div className="fixed top-4 left-4 cursor-pointer">
-          <FaBars className="text-white text-xl" onClick={toggleSliderVisibility} />
-        </div>
-      )}
+      <div className="md:hidden fixed top-4 left-4 cursor-pointer">
+        <FaBars className="text-white text-xl" onClick={toggleSliderVisibility} />
+      </div>
 
       <div className={`relative bg-gray-800 text-white h-screen w-64 flex flex-col bg-secondary-dash text-secondary-icon transition-transform duration-500 ${isSliderVisible ? 'translate-x-0' : '-translate-x-full'}`}>
         <div className="p-4 text-center">
@@ -38,15 +36,19 @@ const SliderBar = () => {
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <li><FaBookMedical/></li>
-            <li><Link href="/dashboard/pacientes" className="block ml-2 transition-all hover:text-yellow-500">Consultas</Link></li>
+            <li><Link href="/dashboard/consultas" className="block ml-2 transition-all hover:text-yellow-500">Consultas</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <li><FaUserAlt/></li>
-            <li><Link href="/dashboard/pacientes" className="block ml-2 transition-all hover:text-yellow-500">Perfil</Link></li>
+            <li><Link href="/dashboard/perfil" className="block ml-2 transition-all hover:text-yellow-500">Perfil</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <li><FaHospitalUser/></li>
             <li><Link href="/admin/registrar" className="block ml-2 transition-all hover:text-yellow-500">Registrar</Link></li>
+          </div>
+          <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
+            <li><FaIdCardAlt/></li>
+            <li><Link href="/dashboard/preguntas" className="block ml-2 transition-all hover:text-yellow-500">Cuestionario</Link></li>
           </div>
         </ul>
         <div className="absolute bottom-0 w-full p-4 flex items-center pl-10 transition-transform duration-300 ease-in-out hover:scale-110">
@@ -54,7 +56,7 @@ const SliderBar = () => {
           <Link href="/admin" className="block transition-all hover:text-yellow-500">Salir</Link>
         </div>
         
-        <div className="absolute top-4 right-4 cursor-pointer md:hidden" onClick={toggleSliderVisibility}>
+        <div className="md:hidden absolute top-4 right-4 cursor-pointer" onClick={toggleSliderVisibility}>
           <FaBars className="text-white text-xl" />
         </div>
       </div>
