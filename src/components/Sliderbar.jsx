e8e5c83e6eec2d9b11f902606aff14e65f005757
Fabian <fabianpon@gmail.com>
2024-05-07 16:46:05 -0700

    Modificacion en la ruta "cuestionario"

--- src/components/Sliderbar.jsx
+++ src/components/Sliderbar.jsx
@@ -23,39 +23,39 @@ const SliderBar = () => {
         </div>
         <ul className="flex-grow">
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaHouse/></li>
+            <li><FaHouse /></li>
             <li><Link href="/dashboard" className="block ml-2 transition-all hover:text-yellow-500">Inicio</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaAddressBook/></li>
+            <li><FaAddressBook /></li>
             <li><Link href="/dashboard/historial" className="block ml-2 transition-all hover:text-yellow-500">Historial</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaAddressCard/></li>
+            <li><FaAddressCard /></li>
             <li><Link href="/dashboard/pacientes" className="block ml-2 transition-all hover:text-yellow-500">Pacientes</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaBookMedical/></li>
+            <li><FaBookMedical /></li>
             <li><Link href="/dashboard/consultas" className="block ml-2 transition-all hover:text-yellow-500">Consultas</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaUserAlt/></li>
+            <li><FaUserAlt /></li>
             <li><Link href="/dashboard/perfil" className="block ml-2 transition-all hover:text-yellow-500">Perfil</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaHospitalUser/></li>
+            <li><FaHospitalUser /></li>
             <li><Link href="/admin/registrar" className="block ml-2 transition-all hover:text-yellow-500">Registrar</Link></li>
           </div>
           <div className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
-            <li><FaIdCardAlt/></li>
-            <li><Link href="/dashboard/preguntas" className="block ml-2 transition-all hover:text-yellow-500">Cuestionario</Link></li>
+            <li><FaIdCardAlt /></li>
+            <li><Link href="/dashboard/cuestionario" className="block ml-2 transition-all hover:text-yellow-500">Cuestionario</Link></li>
           </div>
         </ul>
         <div className="absolute bottom-0 w-full p-4 flex items-center pl-10 transition-transform duration-300 ease-in-out hover:scale-110">
-          <FaLock className="mr-2"/>
+          <FaLock className="mr-2" />
           <Link href="/admin" className="block transition-all hover:text-yellow-500">Salir</Link>
         </div>
-        
+
         <div className="md:hidden absolute top-4 right-4 cursor-pointer" onClick={toggleSliderVisibility}>
           <FaBars className="text-white text-xl" />
         </div>
