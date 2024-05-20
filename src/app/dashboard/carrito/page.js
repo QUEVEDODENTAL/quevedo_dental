18b204e993686789a84d451b5ce60ff65a328e48
LuisMurSal <al19760547@ite.edu.mx>
2024-05-20 00:32:25 -0700

    Historial

--- src/app/dashboard/carrito/page.js
+++ src/app/dashboard/carrito/page.js
@@ -1,23 +1,27 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import TarjetaUsuario from '@/components/TarjetaUsuario';
+import Servicios from '@/app/dashboard/servicios/page';
 
-const Pacientes = () => {
+const Carrito = () => {
+  const [carritoSeleccionado, setCarritoSeleccionado] = useState(null);
+
+  // Función para manejar la selección de carrito
+  const handleSeleccionCarrito = (nombreCarrito) => {
+    setCarritoSeleccionado(nombreCarrito);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mx-4 px-4 py-8">
       <h1 className="text-3xl font-bold mb-10">¡Selecciona el carrito del paciente!</h1>
       <div className="w-full md:w-[70%]">
-        <TarjetaUsuario nombre="Juan Pérez" edad={35} href="/dashboard/carrito/juan-perez" />
-        <TarjetaUsuario nombre="María López" edad={42} href="/dashboard/carrito/maria-lopez" />
-        <TarjetaUsuario nombre="Carlos García" edad={28} href="/dashboard/carrito/carlos-garcia" />
-        <TarjetaUsuario nombre="Juan Pérez" edad={35} href="/dashboard/carrito/juan-perez" />
-        <TarjetaUsuario nombre="María López" edad={42} href="/dashboard/carrito/maria-lopez" />
-        <TarjetaUsuario nombre="Carlos García" edad={28} href="/carrito/carlos-garcia" />
-        <TarjetaUsuario nombre="Juan Pérez" edad={35} href="/dashboard/carrito/juan-perez" />
-        <TarjetaUsuario nombre="María López" edad={42} href="/dashboard/carrito/maria-lopez" />
-        <TarjetaUsuario nombre="Carlos García" edad={28} href="/dashboard/carrito/carlos-garcia" />
+        <TarjetaUsuario nombre="Juan Pérez" edad={35} onClick={() => handleSeleccionCarrito("juan-perez")} />
+        <TarjetaUsuario nombre="María López" edad={42} onClick={() => handleSeleccionCarrito("maria-lopez")} />
+        <TarjetaUsuario nombre="Carlos García" edad={28} onClick={() => handleSeleccionCarrito("carlos-garcia")} />
       </div>
+      <Servicios carritoSeleccionado={carritoSeleccionado} />
     </div>
   );
 }
 
-export default Pacientes;
+export default Carrito;
