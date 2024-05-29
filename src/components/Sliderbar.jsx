eb923398f0b8899a0a602395dede78c4829eab08
LuisMurSal <al19760547@ite.edu.mx>
2024-05-29 12:47:14 -0700

    Estado "Cargando" en cada una de las opciones del sliderbar

--- src/components/Sliderbar.jsx
+++ src/components/Sliderbar.jsx
@@ -6,13 +6,30 @@ import { FaHouse, FaAddressCard, FaHospitalUser, FaBookMedical } from "react-ico
 import { signOut } from 'next-auth/react';
 
 const SliderBar = ({ isSliderVisible, toggleSliderVisibility }) => {
+  const [loading, setLoading] = useState(null); // Estado de carga para la opción seleccionada
 
   const handleSignOut = async () => {
     try {
+      setLoading('signOut'); // Establecer estado de carga
       const data = await signOut({ redirect: false, callbackUrl: '/auth/login' });
       window.location.href = data.url;
     } catch (error) {
       console.error('Error during sign out:', error);
+    } finally {
+      setLoading(null); // Restablecer estado de carga
+    }
+  };
+
+  const handleLinkClick = async (option) => {
+    setLoading(option); // Establecer estado de carga para la opción seleccionada
+    // Lógica adicional para cada opción del menú
+    try {
+      // Simular una carga de 1 segundo
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setLoading(null); // Restablecer estado de carga
     }
   };
 
@@ -32,27 +49,39 @@ const SliderBar = ({ isSliderVisible, toggleSliderVisibility }) => {
         <ul className="flex-grow">
           <li className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <FaHouse />
-            <Link href="/dashboard" className="block ml-2 transition-all hover:text-yellow-500">Inicio</Link>
+            <Link href="/dashboard" className="block ml-2 transition-all hover:text-yellow-500" onClick={() => handleLinkClick('dashboard')}>
+              {loading === 'dashboard' ? 'Cargando...' : 'Inicio'}
+            </Link>
           </li>
           <li className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <FaAddressCard />
-            <Link href="/dashboard/pacientes" className="block ml-2 transition-all hover:text-yellow-500">Pacientes</Link>
+            <Link href="/dashboard/pacientes" className="block ml-2 transition-all hover:text-yellow-500" onClick={() => handleLinkClick('pacientes')}>
+              {loading === 'pacientes' ? 'Cargando...' : 'Pacientes'}
+            </Link>
           </li>
           <li className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <FaUserAlt />
-            <Link href="/dashboard/mi_perfil" className="block ml-2 transition-all hover:text-yellow-500">Perfil</Link>
+            <Link href="/dashboard/mi_perfil" className="block ml-2 transition-all hover:text-yellow-500" onClick={() => handleLinkClick('mi_perfil')}>
+              {loading === 'mi_perfil' ? 'Cargando...' : 'Perfil'}
+            </Link>
           </li>
           <li className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <FaHospitalUser />
-            <Link href="/dashboard/registrar" className="block ml-2 transition-all hover:text-yellow-500">Registrar</Link>
+            <Link href="/dashboard/registrar" className="block ml-2 transition-all hover:text-yellow-500" onClick={() => handleLinkClick('registrar')}>
+              {loading === 'registrar' ? 'Cargando...' : 'Registrar'}
+            </Link>
           </li>
           <li className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <FaIdCardAlt />
-            <Link href="/dashboard/cuestionario" className="block ml-2 transition-all hover:text-yellow-500">Cuestionario</Link>
+            <Link href="/dashboard/cuestionario" className="block ml-2 transition-all hover:text-yellow-500" onClick={() => handleLinkClick('cuestionario')}>
+              {loading === 'cuestionario' ? 'Cargando...' : 'Cuestionario'}
+            </Link>
           </li>
           <li className="p-4 pl-10 flex items-center transition-transform duration-300 ease-in-out hover:scale-110">
             <FaBookMedical />
-            <Link href="/dashboard/servicios" className="block ml-2 transition-all hover:text-yellow-500">Servicios</Link>
+            <Link href="/dashboard/servicios" className="block ml-2 transition-all hover:text-yellow-500" onClick={() => handleLinkClick('servicios')}>
+              {loading === 'servicios' ? 'Cargando...' : 'Servicios'}
+            </Link>
           </li>
           <li className="bottom-0 mt-[20%] p-4 flex items-center pl-10 transition-transform duration-300 ease-in-out hover:scale-110">
             <FaLock className="mr-2" />
