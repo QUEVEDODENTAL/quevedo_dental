d2e5d862049b8bec87e0053670fc3e2752330fec
LuisMurSal <al19760547@ite.edu.mx>
2024-05-29 12:22:02 -0700

    Boton de entrada cargando en el login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,51 +1,39 @@
 'use client'
 
-// Importación de módulos y hooks necesarios
-import { set, useForm } from "react-hook-form"; // Para el manejo de formularios
-import { signIn } from "next-auth/react"; // Para la autenticación
-import { useRouter } from 'next/navigation'; // Para la navegación
-import { useState } from 'react'; // Para manejar el estado
+import { set, useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-// Componente principal del formulario de inicio de sesión
 function LoginForm() {
-  // Inicialización de useForm para manejar el formulario
-  const {
-    register, // Para registrar los campos del formulario
-    handleSubmit, // Para manejar el envío del formulario
-    formState: { errors }, // Para manejar los errores del formulario
-  } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const router = useRouter();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false); // Variable de estado para controlar el estado de carga del botón
 
-  const router = useRouter(); // Hook para la navegación
-  const [error, setError] = useState(null); // Estado para manejar errores de autenticación
-
-  // Función que se ejecuta al enviar el formulario
   const onSubmit = handleSubmit(async (data) => {
-    // console.log(data);
+    setLoading(true); // Establecer el estado de carga al enviar el formulario
 
-    // Intento de inicio de sesión utilizando las credenciales proporcionadas
     const res = await signIn('credentials', {
-      Email: data.Email, // Email del usuario
-      Password: data.Password, // Contraseña del usuario
-      redirect: false, // No redirigir automáticamente
+      Email: data.Email,
+      Password: data.Password,
+      redirect: false,
     });
 
-    // console.log(res);
-
-    // Manejo de errores o redirección en caso de éxito
     if (res.error) {
-      setError(res.error); // Establece el error si ocurre
+      setError(res.error);
     } else {
-      router.push('/dashboard'); // Redirige al dashboard si el inicio de sesión es exitoso
+      router.push('/dashboard');
     }
+
+    setLoading(false); // Restablecer el estado de carga después de recibir la respuesta
   });
 
   return (
-    <div className="mx-auto p-8 w-4/5 max-w-lg custom-shadow rounded-lg bg-secondary-dash2"
-    >
+    <div className="mx-auto p-8 w-4/5 max-w-lg custom-shadow rounded-lg bg-secondary-dash2">
       <h1 className='text-5xl font-bold flex justify-center mb-8'>Iniciar sesión</h1>
 
       <form onSubmit={onSubmit} className="space-y-8">
-
         {error && (
           <p className="bg-primary-red text-xs text-white p-3 rounded-md text-center"> {error}</p>
         )}
@@ -59,7 +47,7 @@ function LoginForm() {
             {...register("Email", {
               required: {
                 value: true,
-                message: "Correo electronico requerido", // Mensaje de error si el campo está vacío
+                message: "Correo electronico requerido",
               },
             })}
             className="w-full outline-none rounded-md mb-4 bg-gray-200 px-4 py-3 text-center"
@@ -79,10 +67,10 @@ function LoginForm() {
           {...register("Password", {
             required: {
               value: true,
-              message: "Contraseña requerida", // Mensaje de error si el campo está vacío
+              message: "Contraseña requerida",
             },
           })}
-          className="w-full outline-none rounded-md bg-gray-200 px-4 py-3 text-center" 
+          className="w-full outline-none rounded-md bg-gray-200 px-4 py-3 text-center"
           placeholder="Ingresa tu contraseña"
           style={{ fontWeight: "300" }}
         />
@@ -92,10 +80,16 @@ function LoginForm() {
           </span>
         )}
 
-        <button  className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg bg-gradient-to-r from-purple-700 to-blue-500"
-        style={{background:"linear-gradient(to right,  #2D16AD, #167CA5, #5AA7E6, #5EDCEC) "}}>
-          Entrar
-        </button>
+        {/* Condición para mostrar el botón de entrada o el estado de carga */}
+        {!loading ? (
+          <button type="submit" className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg bg-gradient-to-r from-purple-700 to-blue-500" style={{background:"linear-gradient(to right,  #2D16AD, #167CA5, #5AA7E6, #5EDCEC) "}}>
+            Entrar
+          </button>
+        ) : (
+          <button className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out bg-gray-400" disabled>
+            Cargando...
+          </button>
+        )}
       </form>
     </div>
   );
